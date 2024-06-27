dcba3eeab11ed6cec74e52b7b589db804efaaa76
Do Duy Dong <doduydong.97d@gmail.com>
2024-06-27 11:55:37 +0700

    Apply Log4j2

--- src/test/java/commons/BaseTest.java
+++ src/test/java/commons/BaseTest.java
@@ -3,6 +3,8 @@ package commons;
 import java.time.Duration;
 import java.util.Random;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -10,8 +12,11 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BaseTest {
 	private WebDriver driver;
+	protected Logger log = LogManager.getLogger(this.getClass());
+	private String browserName;
 
 	protected WebDriver initDriver(String browserName, String siteName, String serverName) {
+		this.browserName = browserName;
 		switch (browserName.toLowerCase()) {
 		case "chrome":
 			driver = new ChromeDriver();
@@ -25,6 +30,7 @@ public class BaseTest {
 		default:
 			throw new RuntimeException("'" + browserName.toUpperCase() + "' Browser is invalid");
 		}
+		log.info("---------- [Run on '" + browserName.toUpperCase() + "' Browser] ----------");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(GlobalConstants.LONG_TIMEOUT));
 		driver.manage().window().maximize();
 		driver.get(getPageUrlOf(siteName, serverName));
@@ -34,6 +40,7 @@ public class BaseTest {
 	protected void quitDriver() {
 		driver.manage().deleteAllCookies();
 		driver.quit();
+		log.info("------------------------- [DONE - " + browserName.toUpperCase() + "] -------------------------");
 	}
 
 	private String getPageUrlOf(String siteName, String serverName) {
@@ -74,6 +81,14 @@ public class BaseTest {
 
 	protected int getRandomNumber() {
 		return new Random().nextInt(99999);
+	}
+
+	protected void verify(Object actual, Object expected) {
+		if (actual.equals(expected)) {
+		} else {
+			log.info("-------------------- [FAILED] --------------------");
+			throw new RuntimeException("expected [" + expected + "] but found [" + actual + "]");
+		}
 	}
 
 }
